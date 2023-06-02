5b5c0a6a20ee71bc6d64455751f802c92dbe3895
RAHUL-700 <117377366+RAHUL-700@users.noreply.github.com>
2023-06-02 12:53:16 +0530

    Update BaseClass.java

--- com.RE.Twinlite/src/main/java/com/Twinlite/genericUtilities/BaseClass.java
+++ com.RE.Twinlite/src/main/java/com/Twinlite/genericUtilities/BaseClass.java
@@ -33,10 +33,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 		public JavaUtilities jus=new JavaUtilities();
 		
 		// connect to DB
-		@BeforeSuite(groups= {"SmokeTest","RegTest"})
+		@BeforeSuite(groups= {"SmokeTest","RegTest"}) 
 		public void Config_BS() throws Throwable
 		{
 			//dLib.connectToDb();
+			Thread.sleep(5000);
 			System.out.println("-- connect to DB---");
 		}
 		
